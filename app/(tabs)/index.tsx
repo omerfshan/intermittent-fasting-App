255d628e8df392b2f13567128179e41ba27c8187
sahan <101819553+omerfshan@users.noreply.github.com>
2026-04-19 19:19:13 +0300

    Remove project configuration files and reset script

--- app/(tabs)/index.tsx
+++
@@ -1,5 +0,0 @@
-import TimerScreen from "@/Screen/TimerScreen";
-
-export default function HomeScreen() {
-  return <TimerScreen />;
-}
--- app/modals/fasting-plan.tsx
+++
@@ -1,5 +0,0 @@
-import FastingPlanScreen from "@/Screen/FastingPlanScreen/FastingPlanScreen";
-
-export default function FastingPlanPage() {
-  return <FastingPlanScreen />;
-}
